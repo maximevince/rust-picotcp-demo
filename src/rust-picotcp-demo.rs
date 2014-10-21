082192b4c73cca103b3df05278a6c02f74aac27b
Daniele Lacamera <daniele.lacamera@tass.be>
2014-10-21 18:35:57 +0200

    Simplified demo - 10LOC

--- src/rust-picotcp-demo.rs
+++ src/rust-picotcp-demo.rs
@@ -1,27 +1,14 @@
 extern crate libc;
 extern crate picotcp;
-
-use std::io::timer;
-use std::time::Duration;
-
 use picotcp::pico_ip4;
 
 fn main() {
-
+    picotcp::stack_init();
     let my_ip_addr = pico_ip4::new("192.168.2.150");
     let my_netmask = pico_ip4::new("255.255.255.0");
-
-    picotcp::stack_init();
-
     let pico_dev_eth = picotcp::tun_create("tun0");
-
     picotcp::ipv4_link_add(pico_dev_eth, my_ip_addr, my_netmask);
     println!("tun0: ip addr is {}", my_ip_addr);
-
-    loop {
-        picotcp::stack_tick();
-        timer::sleep(Duration::milliseconds(1));
-    }
-
+    picotcp::stack_loop();
 }
 
